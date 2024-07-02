06ce493d1ed408a940b94f89326c86219a708d7b
hilary-aoao <69435811+hilary-aoao@users.noreply.github.com>
2024-07-02 13:54:15 +0100

    chore: Update code layout

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ setInterval(()=>{
     const currentTime = currentDate.toUTCString().slice(17,25)
     document.getElementById("currentTime").textContent = currentTime
 }, 100)
+
+const date = new Date();
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDayOfWeek = daysOfWeek[date.getDay()];
+
+document.getElementById("currentDayOfWeek").textContent = currentDayOfWeek
